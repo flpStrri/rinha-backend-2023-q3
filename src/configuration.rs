59079a6968514b03f224db5bfe294fa757e6434f
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2023-10-16 16:12:51 -0300

    feat: dockerized

--- src/configuration.rs
+++ src/configuration.rs
@@ -23,12 +23,49 @@ impl DatabaseConfiguration {
 }
 
 pub fn get_static_configuration() -> Result<StaticConfiguration, config::ConfigError> {
-    let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
-        ))
-        .build()?;
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
 
-    settings.try_deserialize::<StaticConfiguration>()
+    config::Config::builder()
+        .add_source(config::File::from(configuration_directory.join("base")).required(true))
+        .add_source(
+            config::File::from(configuration_directory.join(environment.as_str())).required(true),
+        )
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
+        .build()?
+        .try_deserialize()
+}
+
+enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Environment::Local),
+            "production" => Ok(Environment::Production),
+            other => Err(format!("{} is not a valid environment", other)),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> Result<(), hyper::Error> {
 
     let static_config = rinha_backend_2023_q3::configuration::get_static_configuration()
         .expect("failed to load configs");
-    let server_address = SocketAddr::from((Ipv4Addr::LOCALHOST, static_config.application_port));
+    let server_address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, static_config.application_port));
     let server_listener = TcpListener::bind(server_address).expect("failed to bind random port");
 
     let mongodb_pool = rinha_backend_2023_q3::get_database_connection(static_config.database)
