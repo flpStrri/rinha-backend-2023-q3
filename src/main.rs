4df4a10f5bb0e1e2f4ec272e47c5910d7e0ccf7a
João Filipe Storarri <13003392+flpStrri@users.noreply.github.com>
2023-10-16 17:29:25 -0300

    feat: share startup between production and tests

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,17 @@
-use std::net::{Ipv4Addr, SocketAddr, TcpListener};
 use tracing_subscriber::EnvFilter;
+
+use rinha_backend_2023_q3::startup::Application;
+use rinha_backend_2023_q3::{configuration, telemetry};
 
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
-    let subscriber = rinha_backend_2023_q3::telemetry::get_subscriber(
+    let subscriber = telemetry::get_subscriber(
         "rinha-de-backend-2023-q3",
         EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info")),
         std::io::stdout,
     );
-    rinha_backend_2023_q3::telemetry::init_subscriber(subscriber);
+    telemetry::init_subscriber(subscriber);
 
-    let static_config = rinha_backend_2023_q3::configuration::get_static_configuration()
-        .expect("failed to load configs");
-    let server_address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, static_config.application_port));
-    let server_listener = TcpListener::bind(server_address).expect("failed to bind random port");
-
-    let mongodb_pool = rinha_backend_2023_q3::get_database_connection(static_config.database)
-        .await
-        .expect("failed to connect to mongodb");
-
-    rinha_backend_2023_q3::run(server_listener, mongodb_pool)
-        .await
-        .unwrap()
-        .await
+    let static_config = configuration::get_static_configuration().expect("failed to load configs");
+    Application::build(static_config).await.run().await
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,11 +1,9 @@
-use std::net::{Ipv4Addr, SocketAddr, TcpListener};
 use std::sync::Once;
 
-use mongodb::Database;
 use tracing_subscriber::EnvFilter;
 
-use rinha_backend_2023_q3::configuration::get_static_configuration;
-use rinha_backend_2023_q3::get_database_connection;
+use rinha_backend_2023_q3::startup::Application;
+use rinha_backend_2023_q3::{configuration, telemetry};
 
 static TRACING: Once = Once::new();
 
@@ -15,41 +13,24 @@ pub async fn spawn_app() -> String {
         let subscriber_name = "rinha-de-backend-2023-q3";
 
         if std::env::var("TEST_LOG").is_ok() {
-            let subscriber = rinha_backend_2023_q3::telemetry::get_subscriber(
-                subscriber_name,
-                default_filter_level,
-                std::io::stdout,
-            );
-            rinha_backend_2023_q3::telemetry::init_subscriber(subscriber);
+            let subscriber =
+                telemetry::get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+            telemetry::init_subscriber(subscriber);
         } else {
-            let subscriber = rinha_backend_2023_q3::telemetry::get_subscriber(
-                subscriber_name,
-                default_filter_level,
-                std::io::sink,
-            );
-            rinha_backend_2023_q3::telemetry::init_subscriber(subscriber);
+            let subscriber =
+                telemetry::get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+            telemetry::init_subscriber(subscriber);
         };
     });
 
-    let test_address = SocketAddr::from((Ipv4Addr::LOCALHOST, 0));
-    let test_listener = TcpListener::bind(test_address).expect("failed to bind random port");
-    let local_address = test_listener.local_addr().unwrap();
-    let database = get_test_database()
-        .await
-        .expect("failed to connect to mongodb");
-
-    let test_server = rinha_backend_2023_q3::run(test_listener, database)
-        .await
-        .expect("failed to run the server");
-
-    tokio::spawn(test_server);
-    format!("http://{}", local_address)
-}
-
-pub async fn get_test_database() -> Result<Database, mongodb::error::Error> {
-    let mut test_config = get_static_configuration().expect("failed to load configs");
+    let mut static_config =
+        configuration::get_static_configuration().expect("failed to load configs");
     let test_database_name = format!("test-{}", &ulid::Ulid::new().to_string());
-    test_config.database.database_name = test_database_name;
+    static_config.database.database_name = test_database_name;
 
-    get_database_connection(test_config.database).await
+    let application = Application::build(static_config).await;
+    let address = format!("http://{}", application.address());
+
+    tokio::spawn(async move { application.run().await.expect("Failed to run the server") });
+    address
 }
